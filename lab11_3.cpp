575f131b8e1e2501a18ccbbfc9c46b51e61caf67
ShinRunner1st <shinrunner1st@gmail.com>
2023-01-03 15:44:24 +0700

    finish

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ifstream source("score.txt");
+    double mean, standarddiv;
+    double sum, sum2, n;
+    string textline;
+
+    while(getline(source,textline))
+    {
+        sum += atof(textline.c_str());
+        sum2 += pow(atof(textline.c_str()),2);
+        n++;
+    }
+
+    mean = sum/n;
+    standarddiv = sqrt((sum2/n)-pow(mean,2));
+
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << standarddiv << endl;
+
+    source.close();
+}
